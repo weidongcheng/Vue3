72ab9651d99a0ff489a5baac8923fa6b825d6ee2
weidongcheng <18621756389@163.com>
2022-06-27 19:14:02 +0800

    学习自定义hook

---
+++ 12_src_自定义hook/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
+  <Demo v-if="isShowDemo"/>
+  <hr>
+  <Test/>
+</template>
+
+<script>
+import Demo from "@/components/Demo";
+import Test from "@/components/Test";
+import {ref} from "vue";
+
+export default {
+  name: 'App',
+  components:{Demo,Test},
+  setup(){
+    let isShowDemo = ref(true)
+    return{isShowDemo}
+  }
+}
+</script>
+
